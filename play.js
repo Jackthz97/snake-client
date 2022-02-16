f73e5b72842688885652b0ee532dc6f07ac08047
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-16 15:22:35 -0500

    use require() to import the connect function from client.js

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const net = require("net");
-const stdin = process.stdin;
+const connect = require('./client');
 
 const name = "jack WORKING";
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: "50541"
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.write("Name: Jak");
-  conn.write('Move: Up');
-  conn.write('Move: DOwn');
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
