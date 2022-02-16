de2ccd1a9087af2bf9505074d8eeadf3cc6f42e1
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-16 17:14:23 -0500

    added in user input movements

--- play.js
+++ play.js
@@ -1,8 +1,35 @@
 const net = require("net");
-const connect = require('./client');
+const client = require('./client');
+const stdin = process.stdin;
+stdin.setEncoding('utf8');
 
-const name = "jack WORKING";
+const setupInput = function() {
+  // const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+const handleUserInput = function(input) {
+  if (input === '\u0003') {
+    process.exit();
+  }
+  if (input === 'w') {
+    client.conn.write('Move: up');
+  }
+  if (input === 's') {
+    client.conn.write('Move: down');
+  }
+  if (input === 'a') {
+    client.conn.write('Move: left');
+  }
+  if (input === 'd') {
+    client.conn.write('Move: right');
+  }
+};
 
 console.log("Connecting ...");
-connect();
+client.connect();
+setupInput();
