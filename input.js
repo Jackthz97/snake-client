f5777b08ed9c6ae7d7266e2b4057c1c34b40c9ab
Tian Hao Zhang <97329471+Jackthz97@users.noreply.github.com>
2022-02-16 17:25:39 -0500

    moved the movement functions into this file and exported the setupInput movement function

---
+++ input.js
@@ -0,0 +1,32 @@
+const client = require('./client');
+const stdin = process.stdin;
+stdin.setEncoding('utf8');
+
+const setupInput = function() {
+  // const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(input) {
+  if (input === '\u0003') {
+    process.exit();
+  }
+  if (input === 'w') {
+    client.conn.write('Move: up');
+  }
+  if (input === 's') {
+    client.conn.write('Move: down');
+  }
+  if (input === 'a') {
+    client.conn.write('Move: left');
+  }
+  if (input === 'd') {
+    client.conn.write('Move: right');
+  }
+};
+
+module.exports = setupInput;
